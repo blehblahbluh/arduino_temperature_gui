d1766aab47bb31c28b234c95af452875578c4051
blehblahbluh <blehblahbluh@users.noreply.github.com>
2016-05-24 18:47:49 +0100

    Add files via upload

---
+++ gui_program/temperature-sensor-GUI/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace temperature_sensor_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.listLEDGrn = new System.Windows.Forms.ListBox();
+            this.listLEDYlw = new System.Windows.Forms.ListBox();
+            this.listLEDRed = new System.Windows.Forms.ListBox();
+            this.btnSwitch = new System.Windows.Forms.Button();
+            this.chrtTemp = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnLEDGrn = new System.Windows.Forms.Button();
+            this.btnInitiate = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.graphWorker = new System.ComponentModel.BackgroundWorker();
+            this.msgLabel = new System.Windows.Forms.Label();
+            this.btnLEDYlw = new System.Windows.Forms.Button();
+            this.btnLEDRed = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chrtTemp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listLEDGrn
+            // 
+            this.listLEDGrn.FormattingEnabled = true;
+            this.listLEDGrn.Items.AddRange(new object[] {
+            "18",
+            "20",
+            "22",
+            "24",
+            "26",
+            "28",
+            "30"});
+            this.listLEDGrn.Location = new System.Drawing.Point(493, 96);
+            this.listLEDGrn.Margin = new System.Windows.Forms.Padding(2);
+            this.listLEDGrn.Name = "listLEDGrn";
+            this.listLEDGrn.Size = new System.Drawing.Size(91, 69);
+            this.listLEDGrn.TabIndex = 1;
+            this.listLEDGrn.SelectedIndexChanged += new System.EventHandler(this.listLEDGrn_SelectedIndexChanged);
+            // 
+            // listLEDYlw
+            // 
+            this.listLEDYlw.FormattingEnabled = true;
+            this.listLEDYlw.Items.AddRange(new object[] {
+            "18",
+            "20",
+            "22",
+            "24",
+            "26",
+            "28",
+            "30"});
+            this.listLEDYlw.Location = new System.Drawing.Point(493, 206);
+            this.listLEDYlw.Margin = new System.Windows.Forms.Padding(2);
+            this.listLEDYlw.Name = "listLEDYlw";
+            this.listLEDYlw.Size = new System.Drawing.Size(91, 69);
+            this.listLEDYlw.TabIndex = 2;
+            // 
+            // listLEDRed
+            // 
+            this.listLEDRed.FormattingEnabled = true;
+            this.listLEDRed.Items.AddRange(new object[] {
+            "18",
+            "20",
+            "22",
+            "24",
+            "26",
+            "28",
+            "30"});
+            this.listLEDRed.Location = new System.Drawing.Point(493, 312);
+            this.listLEDRed.Margin = new System.Windows.Forms.Padding(2);
+            this.listLEDRed.Name = "listLEDRed";
+            this.listLEDRed.Size = new System.Drawing.Size(91, 69);
+            this.listLEDRed.TabIndex = 3;
+            // 
+            // btnSwitch
+            // 
+            this.btnSwitch.Location = new System.Drawing.Point(302, 440);
+            this.btnSwitch.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSwitch.Name = "btnSwitch";
+            this.btnSwitch.Size = new System.Drawing.Size(106, 40);
+            this.btnSwitch.TabIndex = 4;
+            this.btnSwitch.Text = "Switch Function";
+            this.btnSwitch.UseVisualStyleBackColor = true;
+            this.btnSwitch.Click += new System.EventHandler(this.btnSwitch_Click);
+            // 
+            // chrtTemp
+            // 
+            chartArea3.Name = "tmpArea";
+            this.chrtTemp.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chrtTemp.Legends.Add(legend3);
+            this.chrtTemp.Location = new System.Drawing.Point(47, 45);
+            this.chrtTemp.Margin = new System.Windows.Forms.Padding(2);
+            this.chrtTemp.Name = "chrtTemp";
+            series3.ChartArea = "tmpArea";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Temp";
+            this.chrtTemp.Series.Add(series3);
+            this.chrtTemp.Size = new System.Drawing.Size(361, 336);
+            this.chrtTemp.TabIndex = 5;
+            this.chrtTemp.Text = "Temperature";
+            this.chrtTemp.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // btnLEDGrn
+            // 
+            this.btnLEDGrn.Location = new System.Drawing.Point(591, 113);
+            this.btnLEDGrn.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLEDGrn.Name = "btnLEDGrn";
+            this.btnLEDGrn.Size = new System.Drawing.Size(90, 40);
+            this.btnLEDGrn.TabIndex = 6;
+            this.btnLEDGrn.Text = "Set Green LED";
+            this.btnLEDGrn.UseVisualStyleBackColor = true;
+            this.btnLEDGrn.Click += new System.EventHandler(this.btnLED_Click);
+            // 
+            // btnInitiate
+            // 
+            this.btnInitiate.Location = new System.Drawing.Point(47, 440);
+            this.btnInitiate.Margin = new System.Windows.Forms.Padding(2);
+            this.btnInitiate.Name = "btnInitiate";
+            this.btnInitiate.Size = new System.Drawing.Size(106, 40);
+            this.btnInitiate.TabIndex = 7;
+            this.btnInitiate.Text = "Initiate";
+            this.btnInitiate.UseVisualStyleBackColor = true;
+            this.btnInitiate.Click += new System.EventHandler(this.btnInitiate_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(175, 440);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(106, 40);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save Data";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // graphWorker
+            // 
+            this.graphWorker.WorkerSupportsCancellation = true;
+            this.graphWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // msgLabel
+            // 
+            this.msgLabel.AutoSize = true;
+            this.msgLabel.Location = new System.Drawing.Point(490, 45);
+            this.msgLabel.Name = "msgLabel";
+            this.msgLabel.Size = new System.Drawing.Size(27, 13);
+            this.msgLabel.TabIndex = 9;
+            this.msgLabel.Text = "N/A";
+            // 
+            // btnLEDYlw
+            // 
+            this.btnLEDYlw.Location = new System.Drawing.Point(591, 220);
+            this.btnLEDYlw.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLEDYlw.Name = "btnLEDYlw";
+            this.btnLEDYlw.Size = new System.Drawing.Size(90, 40);
+            this.btnLEDYlw.TabIndex = 10;
+            this.btnLEDYlw.Text = "Set Yellow LED";
+            this.btnLEDYlw.UseVisualStyleBackColor = true;
+            this.btnLEDYlw.Click += new System.EventHandler(this.btnLEDYlw_Click);
+            // 
+            // btnLEDRed
+            // 
+            this.btnLEDRed.Location = new System.Drawing.Point(591, 327);
+            this.btnLEDRed.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLEDRed.Name = "btnLEDRed";
+            this.btnLEDRed.Size = new System.Drawing.Size(90, 40);
+            this.btnLEDRed.TabIndex = 11;
+            this.btnLEDRed.Text = "Set Red LED";
+            this.btnLEDRed.UseVisualStyleBackColor = true;
+            this.btnLEDRed.Click += new System.EventHandler(this.btnLEDRed_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 579);
+            this.Controls.Add(this.btnLEDRed);
+            this.Controls.Add(this.btnLEDYlw);
+            this.Controls.Add(this.msgLabel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnInitiate);
+            this.Controls.Add(this.btnLEDGrn);
+            this.Controls.Add(this.chrtTemp);
+            this.Controls.Add(this.btnSwitch);
+            this.Controls.Add(this.listLEDRed);
+            this.Controls.Add(this.listLEDYlw);
+            this.Controls.Add(this.listLEDGrn);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Arduino Central";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chrtTemp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listLEDGrn;
+        private System.Windows.Forms.ListBox listLEDYlw;
+        private System.Windows.Forms.ListBox listLEDRed;
+        private System.Windows.Forms.Button btnSwitch;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chrtTemp;
+        private System.Windows.Forms.Button btnLEDGrn;
+        private System.Windows.Forms.Button btnInitiate;
+        private System.Windows.Forms.Button btnSave;
+        private System.ComponentModel.BackgroundWorker graphWorker;
+        private System.Windows.Forms.Label msgLabel;
+        private System.Windows.Forms.Button btnLEDYlw;
+        private System.Windows.Forms.Button btnLEDRed;
+    }
+}
+
---
+++ gui_program/temperature-sensor-GUI/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Threading;
+
+namespace temperature_sensor_GUI
+{
+    public partial class Form1 : Form
+    {
+        int tmpDataLoc = 0;
+        List<string> tmpData = new List<string>();
+        SerialPort arduinoBoard = new SerialPort("COM3");
+        string boardFunction = "one";
+        public Form1()
+        {
+            InitializeComponent();
+            if (!arduinoBoard.IsOpen)
+            {
+                arduinoBoard.Open();
+            }
+            else
+            {
+                throw new InvalidOperationException("The Serial Port is already open!");
+            }
+            listLEDGrn.SetSelected(2, true);
+            listLEDYlw.SetSelected(3, true);
+            listLEDRed.SetSelected(4, true);
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            List<string> saveData = tmpData;
+            //for (int i = 0; i < saveData.Capacity; i++)
+            //{
+
+            //}
+            System.IO.File.WriteAllLines(@"C:\Users\Michael\Documents\Temp\tmp.txt", saveData);
+        }
+
+        public void tester()
+        {
+            if (tmpDataLoc < 15)
+            {
+                chrtTemp.Series["Temp"].Points.AddY(tmpData[tmpDataLoc]); //needs editing
+                msgLabel.Text = tmpDataLoc.ToString();
+                //msgLabel.Text = tmpData[tmpDataLoc];
+            }
+            else if (tmpDataLoc >= 15)
+            {
+                chrtTemp.Series["Temp"].Points.RemoveAt(0);
+                chrtTemp.Series["Temp"].Points.AddY(tmpData[tmpDataLoc]);
+                msgLabel.Text = tmpDataLoc.ToString();
+                //msgLabel.Text = tmpData[tmpDataLoc];
+            }
+        }
+
+        public void tester2()
+        {
+            chrtTemp.Series["Temp"].Points.AddY(tmpData[tmpDataLoc]);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Stopwatch stopWatch = new Stopwatch();
+            //stopWatch.Start();
+            //var time = (int)stopWatch.Elapsed.TotalSeconds;
+            chrtTemp.ChartAreas["tmpArea"].AxisX.Minimum = 0;
+            chrtTemp.ChartAreas["tmpArea"].AxisX.Maximum = 15;
+            chrtTemp.ChartAreas["tmpArea"].AxisX.Interval = 1;
+            chrtTemp.ChartAreas["tmpArea"].AxisY.Minimum = 0;
+            chrtTemp.ChartAreas["tmpArea"].AxisY.Maximum = 30;
+            chrtTemp.ChartAreas["tmpArea"].AxisY.Interval = 1;
+            chrtTemp.ChartAreas["tmpArea"].AxisX.Title = "Time [s]";
+            chrtTemp.ChartAreas["tmpArea"].AxisY.Title = "Temperature [°C]";
+        }
+
+        private void btnInitiate_Click(object sender, EventArgs e)
+        {
+            if (!graphWorker.IsBusy)
+            {
+                graphWorker.RunWorkerAsync();
+                btnInitiate.Text = "Stop";
+            }
+            else if (graphWorker.IsBusy)
+            {
+                graphWorker.CancelAsync();
+                btnInitiate.Text = "Initiate";
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (arduinoBoard.IsOpen)
+            {
+                tmpData.Add(arduinoBoard.ReadExisting());
+                //chrtTemp.Series["Temp"].Points.AddY(tmpData[tmpDataLoc]); //needs editing
+                MethodInvoker updateGraphDelegate = new MethodInvoker(tester);
+                Invoke(updateGraphDelegate);
+                tmpDataLoc++;
+                Thread.Sleep(1000);
+                if (graphWorker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listLEDGrn_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLED_Click(object sender, EventArgs e)
+        {
+            int setLEDGrn = int.Parse(listLEDGrn.SelectedItem.ToString());
+            if (arduinoBoard.IsOpen)
+            {
+                arduinoBoard.Write("Grn" + setLEDGrn);
+            }
+        }
+
+        private void btnSwitch_Click(object sender, EventArgs e)
+        {
+            if (arduinoBoard.IsOpen && boardFunction == "one")
+            {
+                string lala = "two";
+                arduinoBoard.Write(lala);
+                boardFunction = lala;
+            }
+            else if (arduinoBoard.IsOpen && boardFunction == "two")
+            {
+                string dada = "one";
+                arduinoBoard.Write(dada);
+                boardFunction = dada;
+            }
+        }
+
+        private void btnLEDYlw_Click(object sender, EventArgs e)
+        {
+            int setLEDYlw = int.Parse(listLEDYlw.SelectedItem.ToString());
+            if (arduinoBoard.IsOpen)
+            {
+                arduinoBoard.Write("Ylw" + setLEDYlw);
+            }
+        }
+
+        private void btnLEDRed_Click(object sender, EventArgs e)
+        {
+            int setLEDRed = int.Parse(listLEDRed.SelectedItem.ToString());
+            if (arduinoBoard.IsOpen)
+            {
+                arduinoBoard.Write("Red" + setLEDRed);
+            }
+        }
+    }
+}
